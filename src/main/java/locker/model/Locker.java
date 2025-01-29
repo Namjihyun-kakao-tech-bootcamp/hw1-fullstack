7e2488aca2af408969c51bc89ed646d1ee4d8a77
NamJihyun99 <namjihyun9936@gmail.com>
2025-01-29 20:21:39 +0900

    feat: Locker class/#3

---
+++ src/main/java/locker/model/Locker.java
@@ -0,0 +1,12 @@
+package locker.model;
+
+public class Locker {
+
+    private final Long id;
+    private final Size size;
+
+    public Locker(Long id, Size size) {
+        this.id = id;
+        this.size = size;
+    }
+}
