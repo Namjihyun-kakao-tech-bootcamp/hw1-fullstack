b18d405505d19444aaf9518d2bf20299e3fca25a
NamJihyun99 <namjihyun9936@gmail.com>
2025-02-03 17:08:41 +0900

    feat: 출력 메시지 상수화/#9

--- src/main/java/locker/view/LockerView.java
+++ src/main/java/locker/view/LockerView.java
@@ -5,34 +5,23 @@ import java.util.Scanner;
 
 public abstract class LockerView extends View {
 
-    public String greet() {
-        return "===================\n안녕하세요.\n물품 보관함 서비스입니다.\n";
-    }
+    public static final String greet = "===================\n안녕하세요.\n물품 보관함 서비스입니다.\n";
+    public static final String menu = "===================\n1. 물품 보관\n2. 물품 회수\n3. 종료\n===================\n";
+    public static final String selectionCommand = "원하는 동작을 선택하세요 >> ";
+    public static final String passwordCommand = "암호를 입력하세요 >> ";
 
-    public String menu() {
-        return "===================\n1. 물품 보관\n2. 물품 회수\n3. 종료\n===================\n";
-    }
-
-    public String selectionCommand() {
-        return "원하는 동작을 선택하세요 >> ";
-    }
-
-    public int readMenuSelection() {
+    public int readMenuInput() {
         Scanner scanner = new Scanner(System.in);
         String answer = scanner.nextLine();
-        validateMenuSelection(answer);
+        validateMenuInput(answer);
         return Integer.parseInt(answer);
     }
 
-    private void validateMenuSelection(String selection) {
-        int number = Integer.parseInt(selection);
+    private void validateMenuInput(String input) {
+        int number = Integer.parseInt(input);
         if (number < 1 || number > 3) {
             throw new IllegalArgumentException("올바른 선택지가 아닙니다.");
         }
-    }
-
-    public String passwordCommand() {
-        return "암호를 입력하세요 >> ";
     }
 
     public String readPasswordInput() {
