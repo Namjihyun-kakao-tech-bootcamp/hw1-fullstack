67fcf42cde3892d1b9ea26707314417d7728213f
NamJihyun99 <namjihyun9936@gmail.com>
2025-02-03 11:47:18 +0900

    feat: LockerService에 LockerRepository 의존성 추가/#5

--- src/main/java/locker/LockerService.java
+++ src/main/java/locker/LockerService.java
@@ -1,5 +1,6 @@
 package locker;
 
+import locker.repository.LockerRepository;
 import locker.util.DateTimeGenerator;
 import locker.util.PasswordGenerator;
 
@@ -7,9 +8,11 @@ public class LockerService {
 
     private final PasswordGenerator passwordGenerator;
     private final DateTimeGenerator dateTimeGenerator;
+    private final LockerRepository lockerRepository;
 
-    public LockerService(PasswordGenerator passwordGenerator, DateTimeGenerator dateTimeGenerator) {
+    public LockerService(PasswordGenerator passwordGenerator, DateTimeGenerator dateTimeGenerator, LockerRepository lockerRepository) {
         this.passwordGenerator = passwordGenerator;
         this.dateTimeGenerator = dateTimeGenerator;
+        this.lockerRepository = lockerRepository;
     }
 }
