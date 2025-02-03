aabece368564c481af61489154f77a02148572c0
NamJihyun99 <namjihyun9936@gmail.com>
2025-02-04 04:04:20 +0900

    refactor: Size 조건 inner class 제거/#13

--- src/main/java/locker/model/Size.java
+++ src/main/java/locker/model/Size.java
@@ -2,27 +2,24 @@ package locker.model;
 
 public enum Size {
 
-    SMALL("45*30*60", new Condition(2, 1000, 1, 500)),
-    MEDIUM("45*60*60", new Condition(2, 2000, 1, 1000)),
-    LARGE("45*90*60", new Condition(2, 3000, 1, 1500));
+    SMALL(2, 1000, 1, 500),
+    MEDIUM(2, 2000, 1, 1000),
+    LARGE(2, 3000, 1, 1500);
 
-    private final String description;
-    private final Condition condition;
+    private final Integer baseMinutes;
+    private final Integer baseFee;
+    private final Integer unitMinutes;
+    private final Integer feePerUnit;
 
-    Size(String description, Condition condition) {
-        this.description = description;
-        this.condition = condition;
+    Size(Integer baseMinutes, Integer baseFee, Integer unitMinutes, Integer feePerUnit) {
+        this.baseMinutes = baseMinutes;
+        this.baseFee = baseFee;
+        this.unitMinutes = unitMinutes;
+        this.feePerUnit = feePerUnit;
     }
 
     public Long calculateFee(Long totalMinutes) {
-        return this.condition.getFee(totalMinutes);
-    }
-
-    private record Condition(Integer baseMinutes, Integer baseFee, Integer unitMinutes, Integer feePerUnit) {
-
-        private Long getFee(Long totalMinutes) {
-            long extraMinutes = (totalMinutes > baseMinutes)? totalMinutes - baseMinutes : 0L;
-            return baseFee + feePerUnit * (long) Math.ceil( 1.0 * extraMinutes / unitMinutes);
-        }
+        long extraMinutes = (totalMinutes > baseMinutes)? totalMinutes - baseMinutes : 0L;
+        return baseFee + feePerUnit * (long) Math.ceil( 1.0 * extraMinutes / unitMinutes);
     }
 }
